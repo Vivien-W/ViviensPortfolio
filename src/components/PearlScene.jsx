0e82b4c7f7c86430e419a6e0880044298ed554fa
Vivien Wallbaum <viviwbaum@gmail.com>
2025-08-02 08:16:01 +0200

    Verbesserung der Perle: veränderte Lichteinstellungen

--- src/components/PearlScene.jsx
+++ src/components/PearlScene.jsx
@@ -1,20 +1,23 @@
 import React from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls } from "@react-three/drei";
+import { Environment, OrbitControls } from "@react-three/drei";
 
 function Pearl() {
   return (
-    <mesh>
+    <mesh castShadow receiveShadow>
       <sphereGeometry args={[1, 64, 64]} />
       <meshStandardMaterial
         color="#fff0f5"               
-        metalness={0.7}              // Für metallischen Schimmer
-        roughness={0.2}              // Glatt & glänzend
+        metalness={0.6}              // Für metallischen Schimmer
+        roughness={0.1}              // Glatt & glänzend
         emissive="#ffeeff"           // Leichtes Leuchten von innen
-        emissiveIntensity={0.2}
-        sheen={2}                    
+        emissiveIntensity={0.25}
+        sheen={1}                    
         side={2}
         sheenColor="#ffffff"
+        iridescence={0.6}
+        iridescenceIOR={1.3}
+        iridescenceThicknessRange={[100, 400]}
       />
     </mesh>
   );
@@ -24,14 +27,18 @@ export default function PearlScene() {
   return (
     <Canvas camera={{ position: [0, 0, 3] }}>
       {/* Lichtquellen */}
-      <ambientLight intensity={0.7} />
-      <directionalLight position={[2, 3, 3]} intensity={1.2} />
+      <ambientLight intensity={0.4} />
+      <directionalLight position={[5,5,5]} intensity={1.5} castShadow/>
 
       {/* Die Perle */}
       <Pearl />
 
+      {/* Realistische Umgebung für Reflexionen */}
+      <Environment preset="studio" />
+
+
       {/* Maussteuerung */}
-      <OrbitControls enableZoom={false} />
+      <OrbitControls autoRotate enableZoom={false} />
     </Canvas>
   );
 }
